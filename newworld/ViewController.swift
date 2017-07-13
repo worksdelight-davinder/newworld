82b2fb19119eaa5f532d4fc1f9702a61ff41d602
worksdelight-davinder <davinder.kaur@worksdelight.com>
2017-07-13 11:00:07 +0530

    new world

--- newworld/ViewController.swift
+++ newworld/ViewController.swift
@@ -13,7 +13,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        print("newworld")
+        print("new world")
     }
 
     override func didReceiveMemoryWarning() {
